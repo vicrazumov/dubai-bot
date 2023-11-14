f4569c614c2bca67f560976954d8ccb7b43f6fb3
Viktor Razumov <viktorr@spotify.com>
2023-11-14 18:35:11 +0100

    add user filter

--- index.mjs
+++ index.mjs
@@ -3,13 +3,13 @@ import { initializeOpenApi, createThread } from "./openai-client.mjs";
 import { initializeTelegramBot } from "./telegram.mjs";
 
 dotenv.config();
-const { OPEN_AI_KEY, ASSISTANT_ID, POLLING_INTERVAL, TELEGRAM_KEY } = process.env;
+const { OPEN_AI_KEY, ASSISTANT_ID, POLLING_INTERVAL, TELEGRAM_KEY, TELEGRAM_ALLOWED_USERS } = process.env;
 
 async function main() {
     await initializeOpenApi(OPEN_AI_KEY, ASSISTANT_ID, POLLING_INTERVAL);
 
     initializeTelegramBot(TELEGRAM_KEY, 
-        (ctx) => ctx.reply('Персональный помощник по нашей логистике. А также рекомендации по различным местам. Просто задайте вопрос!'),
+        (ctx) => ctx.reply('Персональный помощник по нашей поездке. А также рекомендации по различным местам. Просто задайте вопрос!'),
         async (ctx) => {
             if (ctx.message.text === '/start') return ctx.reply('Персональный помощник по нашей логистике. А также рекомендации по различным местам. Просто задайте вопрос!');
             const thread = await createThread();            
@@ -20,7 +20,8 @@ async function main() {
             replies.forEach(r => {
                 ctx.sendMessage(r)
             })
-        }
+        },
+        TELEGRAM_ALLOWED_USERS.split(',')
     );
 }
 
--- telegram.mjs
+++ telegram.mjs
@@ -1,14 +1,25 @@
 import { Telegraf } from "telegraf"
 
 let bot;
+const allowedUsernames = [];
+const isUserAllowed = (ctx) => {
+    if (allowedUsernames.indexOf(ctx.from.username) === -1) {
+        console.log(`user ${ctx.from.username} not allowed`);
+        ctx.reply('Your user is not whitelisted');
+        return false;
+    }
+    return true;
+}
 
-export function initializeTelegramBot(apiKey, onStart, onMessage) {
+export function initializeTelegramBot(apiKey, onStart, onMessage, _allowedUsernames) {
     if (bot) {
         console.warn('Telegram bot already connected')
         return
     }
 
     bot = new Telegraf(apiKey)
+
+    _allowedUsernames.forEach(u => allowedUsernames.push(u));
 
     // error handling
     bot.catch((err, ctx) => {
@@ -17,8 +28,17 @@ export function initializeTelegramBot(apiKey, onStart, onMessage) {
     })
 
     // initialize the commands
-    bot.command('/start', onStart)    
-    bot.on('message', onMessage)
+    bot.command('/start', ctx => {
+        if (isUserAllowed(ctx)) {
+            onStart(ctx);
+        }
+    })    
+    bot.on('message', ctx => {
+        if (isUserAllowed(ctx)) {
+            onMessage(ctx);
+        }
+    })
+    
 
     bot.launch()
 
