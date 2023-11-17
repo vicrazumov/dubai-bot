17e90cb7a93b5f09bb2d7f635b0a4fd2e8004aa5
Viktor Razumov <viktorr@spotify.com>
2023-11-17 17:55:28 +0100

    Read settings from db

--- index.mjs
+++ index.mjs
@@ -5,39 +5,61 @@ import { initializeTelegramBot } from "./telegram.mjs";
 import { logger } from "firebase-functions";
 import { onRequest } from "firebase-functions/v2/https";
 import { defineString } from "firebase-functions/params";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore,  } from "firebase-admin/firestore";
+
+initializeApp();
 
 dotenv.config();
-let OPEN_AI_KEY, TELEGRAM_KEY, TELEGRAM_ALLOWED_USERS, WEBHOOK_DOMAIN, TAVILY_KEY, SHEET_DB_KEY, TELEGRAM_STAY_WITH_US_TIMEOUT;
+let OPEN_AI_KEY, TELEGRAM_KEY, TELEGRAM_ALLOWED_USERS, WEBHOOK_DOMAIN, TAVILY_KEY, SHEET_DB_KEY, TELEGRAM_STAY_WITH_US_TIMEOUT, MODEL_INSTRUCTIONS;
 let botLogger;
 let IS_GCLOUD = false;
 
 if (!process.env['GCLOUD_PROJECT']) {
     OPEN_AI_KEY = process.env.OPEN_AI_KEY;
     TELEGRAM_KEY = process.env.TELEGRAM_KEY;
-    TELEGRAM_ALLOWED_USERS = process.env.TELEGRAM_ALLOWED_USERS;
+    TELEGRAM_ALLOWED_USERS = process.env.TELEGRAM_ALLOWED_USERS.split(',');
     WEBHOOK_DOMAIN = process.env.WEBHOOK_DOMAIN;
     TAVILY_KEY = process.env.TAVILY_KEY;
     SHEET_DB_KEY = process.env.SHEET_DB_KEY;
-    TELEGRAM_STAY_WITH_US_TIMEOUT = process.env.TELEGRAM_STAY_WITH_US_TIMEOUT;
+    TELEGRAM_STAY_WITH_US_TIMEOUT = process.env.TELEGRAM_STAY_WITH_US_TIMEOUT;        
 
     botLogger = console;
 } else {    
+    botLogger = logger;
+
     OPEN_AI_KEY = defineString("OPEN_AI_KEY").value();
     TELEGRAM_KEY = defineString("TELEGRAM_KEY").value();
-    TELEGRAM_ALLOWED_USERS = defineString("TELEGRAM_ALLOWED_USERS").value();
+    TELEGRAM_ALLOWED_USERS = defineString("TELEGRAM_ALLOWED_USERS").value().split(',');
     WEBHOOK_DOMAIN = defineString("WEBHOOK_DOMAIN").value();
     TAVILY_KEY = defineString("TAVILY_KEY").value();
     SHEET_DB_KEY = defineString("SHEET_DB_KEY").value();
     TELEGRAM_STAY_WITH_US_TIMEOUT = defineString("TELEGRAM_STAY_WITH_US_TIMEOUT").value();
+
+    const SETTINGS_COLLECTION = defineString("SETTINGS_COLLECTION").value();
     IS_GCLOUD = true;
     
-    botLogger = logger;
+    try {
+        const settingsFromDbRef = await getFirestore().collection(SETTINGS_COLLECTION).get();
+        settingsFromDbRef.docs.forEach(doc => { 
+            switch (doc.id) {
+                case 'TELEGRAM_STAY_WITH_US_TIMEOUT': TELEGRAM_STAY_WITH_US_TIMEOUT = doc.data().value; break;
+                case 'TELEGRAM_ALLOWED_USERS': TELEGRAM_ALLOWED_USERS = doc.data().value; break;
+                case 'MODEL_INSTRUCTIONS': MODEL_INSTRUCTIONS = doc.data().value; break;
+            }
+        });
+
+        botLogger.info("successdully read settings from the db.");
+    } catch (err) {
+        botLogger.error("couldn't read settings from the db. will continue with default settings");
+    }
+    
 }
 
 const greeting = 'Персональный помощник по нашей поездке. А также рекомендации по различным местам. Просто задайте вопрос!';
 
 
-initializeOpenApi(OPEN_AI_KEY, botLogger, TAVILY_KEY, SHEET_DB_KEY);
+initializeOpenApi(OPEN_AI_KEY, botLogger, TAVILY_KEY, SHEET_DB_KEY, MODEL_INSTRUCTIONS);
 
 const bot = initializeTelegramBot(TELEGRAM_KEY, 
     (ctx) => ctx.reply(greeting),
@@ -49,18 +71,25 @@ const bot = initializeTelegramBot(TELEGRAM_KEY,
 
         await ctx.persistentChatAction('typing', async () => {
             try {
-                const timer = setTimeout(() => ctx.reply('Я все еще работаю над вашим запросом. Подождите, пожалуйста, еще'), TELEGRAM_STAY_WITH_US_TIMEOUT);
+                const timer = setTimeout(() => ctx.reply('Я все еще работаю над вашим запросом. Подождите, пожалуйста, еще.'), TELEGRAM_STAY_WITH_US_TIMEOUT);
 
                 const replies = await sendMessageAndGetAnswer(ctx.message.text, userId);
                 clearTimeout(timer);
-                ctx.sendMessage(replies);
+
+                try {
+                    await ctx.sendMessage(replies, { parse_mode: 'Markdown' });
+                } catch (err) {
+                    botLogger.error(`sending markdown failed. sent as plain text`, replies)
+
+                    ctx.sendMessage(replies);
+                }
             } catch (err) {
                 ctx.reply("Произошла ошибка. Попробуйте отправить ваш запрос еще раз.")
                 botLogger.info("User informed about the error", err);
             }            
         });        
     },
-    TELEGRAM_ALLOWED_USERS.split(','),
+    TELEGRAM_ALLOWED_USERS,
     botLogger,
     IS_GCLOUD,
 );
--- openai-chat-completion.mjs
+++ openai-chat-completion.mjs
@@ -5,11 +5,13 @@ import { initializeRecommendationEngine, querySheetDb, enums } from "./recommend
 let openai;
 let logger;
 
+let modelInstructions = "Это гид для поездки с семъей в Дубай с 16 по 25 февраля 2024 года.\n\nНаша валюта: RUB.\n\n"
+
 const threads = {
 
 };
 
-export function initializeOpenApi(apiKey, botLogger, tavilyKey, sheetDbKey) {
+export function initializeOpenApi(apiKey, botLogger, tavilyKey, sheetDbKey, _modelInstructions) {
     if (openai) {
         logger.warn('open ai client already initiated')
         return ;
@@ -21,6 +23,8 @@ export function initializeOpenApi(apiKey, botLogger, tavilyKey, sheetDbKey) {
     initializeTavily(tavilyKey, botLogger);
 
     initializeRecommendationEngine(sheetDbKey, botLogger);
+
+    modelInstructions = _modelInstructions;
     
     logger.info('open ai client launched')
 }
@@ -77,7 +81,7 @@ const tools = [
           },
           address: {
             type: "string",
-            description: "Address of the venue.",
+            description: "Address of the venue. If asked JBR or Dubai Marina, just use 'JBR'. If downtown or Burj khalifa, use 'downtown'. If mall of emirates, use 'mall of emirates'",
           },
           limit: {
             type: "integer",
@@ -97,7 +101,7 @@ export async function sendMessageAndGetAnswer(message, thread) {
     let messages = [
       {
         "role": "system",
-        "content": "Это гид для поездки с семъей в Дубай с 16 по 25 февраля 2024 года.\n\nНаша валюта: RUB.\n\nБилеты для родителей из России:\nDeparture from Yekaterinburg (Flight FZ 998) 16 February 2024, Friday 16:45 arrives 21:10 to Dubai International Airport Terminal 2\nDeparture from Dubai International Airport Terminal 2 (Flight FZ 997) 25 February 2024, Sunday 09:30 arrives 15:45 to Yekaterinburg\n\nБилеты для Ани, Вити и Льва из Нидерландов:\nDeparture from Amsterdam (Flights AF1441, AF0658) 16 February 2024, Friday 20:25 arrives 09:20 17 February 2024 to Dubai International Airport Terminal 1\nDeparture from Dubai International Airport Terminal 1 (Flights AF0659, AF1440) 25 February 2024, Sunday 11:10 arrives 19:35 to Amsterdam\n\nНаши апартаменты: Apartment 3203 Al Bateen Residence Towers JBR walk, Jumeirah Beach Residence, Dubai, United Arab Emirates. \nОписание: Stunning 5* 4BR-Oceanfront-Apartment is located in Dubai and provides accommodation with a private beach area and free WiFi, 1.9 km from The Walk at JBR and 3.2 km from Dubai Marina Mall.\nOffering two furnished balconies, all units are air conditioned and feature a dining area and a seating area with a cable flat-screen TV. There is also a kitchen in all apartments, equipped with a dishwasher and an oven.\nGuests can make use of the fitness centre. Guests can enjoy access to a private beach with a licensed beach bar. Guests have access to a large chilled 50m infinity pool and shaded children’s pool with lifeguard.\nСайт: https://www.booking.com/hotel/ae/stunning-5-4br-oceanfront-apartment.en-gb.html?label=gen173nr-1FCAEoggI46AdIM1gEaKkBiAEBmAEJuAEHyAEM2AEB6AEB-AENiAIBqAIDuAKipMmqBsACAdICJGQwNzI4ZDFjLTkyYmYtNGVjYy1hNGYxLWI2ZDk2MDhhNTJkYdgCBuACAQ&sid=b7d01c1beed282e86e8b4516ad085941&aid=304142 \n\nПрокат машины: у нас будет арендована 5-местная машина типа Toyota Corolla. При необходимости будем брать такси.\n\nКуда хочет пойти Лев:\nLegoland Dubai\nOliOli Dubai\nAir Maniax Dubai\nKidzania Dubai Mall\nFerrari World\n\nКуда хотят сходить Аня и Витя:\nLouvre Abu-Dhabi\nAura sky lounge\nMuseum of future\nFrame\nBurj Khalifa\nSki Dubai\nDune bashing\nAya\nOld Dubai \nUntold Festival\nOpera\nMiracle garden\nCloud 22\nCrocodile farm"
+        "content": modelInstructions,
       }      
     ];
 
